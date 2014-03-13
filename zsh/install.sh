873bf9504c7357564448483a6d001b8df737611c
Hauke Schade <cnlpete@cnlpete.de>
2014-03-13 13:16:37 +0100

    / ‘zsh/install.sh’

--- zsh/install.sh
+++ zsh/install.sh
@@ -39,7 +39,7 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 
 setopt EXTENDED_GLOB
-for rcfile in "$dir"/zsh/^install.sh(.N); do
+for rcfile in "${dir}"/zsh/^install.sh(.N); do
   [ ! -L "${ZDOTDIR:-$HOME}/.${rcfile:t}" ] && ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
