2476d2cb1c095492045fe8f0fb6705cdf53d0f39
Hauke Schade <cnlpete@cnlpete.de>
2014-03-13 15:15:15 +0100

    / ‘zsh/install.sh’

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,7 +10,7 @@ install_zsh () {
       #fi
       echo "todo get zprezto"
       # Set the default shell to zsh if it isn't currently set to zsh
-      if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+      if [ ! $(echo $SHELL) = $(which zsh) ]; then
           chsh -s $(which zsh)
       fi
   else
