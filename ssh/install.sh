06d2286cbd85d70987f1dba0a316d2ead02de31a
Hauke Schade <cnlpete@cnlpete.de>
2014-03-13 13:16:52 +0100

    / ‘ssh/install.sh’

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[ ! -L ~/.ssh/config ] && mv ~/.ssh/config $olddir/ssh.config && ln -s $dir/ssh/config ~/.ssh/config && echo "ssh/config"
+[ ! -L ~/.ssh/config ] && mv ~/.ssh/config ${olddir}/ssh.config && ln -s ${dir}/ssh/config ~/.ssh/config && echo "ssh/config"
 
