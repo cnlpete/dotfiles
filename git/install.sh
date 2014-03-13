18e96fd4f7b8b6a339fbaea10e0a47c04b8ac4d7
Hauke Schade <cnlpete@cnlpete.de>
2014-03-13 13:17:25 +0100

    / ‘git/install.sh’

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[ ! -L ~/.gitconfig ] && mv ~/.gitconfig $olddir/gitconfig && ln -s $dir/git/gitconfig ~/.gitconfig && echo "git/config"
+[ ! -L ~/.gitconfig ] && mv ~/.gitconfig ${olddir}/gitconfig && ln -s ${dir}/git/gitconfig ~/.gitconfig && echo "git/config"
 
