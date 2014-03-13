aa8c308edbd73fbf92a391a5c48b394916c9facd
Hauke Schade <cnlpete@cnlpete.de>
2014-03-13 15:14:14 +0100

    / ‘zsh/install.sh’

--- zsh/install.sh
+++ zsh/install.sh
@@ -17,7 +17,7 @@ install_zsh () {
       # If zsh isn't installed, get the platform of the current machine
       platform=$(uname);
       # If the platform is Linux, try an apt-get to install zsh and then recurse
-      if [[ $platform == 'Linux' ]]; then
+      if [ $platform = 'Linux' ]; then
           sudo apt-get install zsh
           install_zsh
       # If the platform is OS X, tell the user to install zsh :)
