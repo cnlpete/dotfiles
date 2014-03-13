6084ef2c812b55b37448549ef41bce7274717cfd
Hauke Schade <cnlpete@cnlpete.de>
2014-03-13 13:17:09 +0100

    / ‘gnupg/install.sh’

--- gnupg/install.sh
+++ gnupg/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-[ ! -L ~/.gnupg/gpg.conf ] && mv ~/.gnupg/gpg.conf $olddir/gnupg.gpg.conf && ln -s $dir/gnupg/gpg.conf ~/.gnupg/gpg.conf && echo "gnupg/config"
+[ ! -L ~/.gnupg/gpg.conf ] && mv ~/.gnupg/gpg.conf ${olddir}/gnupg.gpg.conf && ln -s ${dir}/gnupg/gpg.conf ~/.gnupg/gpg.conf && echo "gnupg/config"
 
