a1e72aa62fa24d930a095132727d299b91669a8c
Hauke Schade <cnlpete@cnlpete.de>
2014-04-01 17:55:35 +0200

    / ‘zsh/install_zsh.sh’

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+# Clone my oh-my-zsh repository from GitHub only if it isn't already present
+if [[ ! -d ~/.zprezto/ ]]; then
+  git clone --recursive https://github.com/cnlpete/prezto.git "~/.zprezto"
+fi
+
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   [ ! -L "${ZDOTDIR:-$HOME}/.${rcfile:t}" ] && ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
